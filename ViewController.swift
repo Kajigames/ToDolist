fe072eb4ed132bc5592acbdd3ff45cdb1689af70
Kajigames <82644262+Kajigames@users.noreply.github.com>
2021-05-12 13:33:30 -0700

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -1,6 +1,8 @@
 import UIKit
 import RealmSwift
 import GoogleMobileAds
+import AdSupport
+import AppTrackingTransparency
 
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource, GADBannerViewDelegate {
@@ -9,47 +11,120 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     var itemList:Results<ToDoItem>!
     var token:NotificationToken!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var GADBannerView: GADBannerView!
     
+    //行番号を取得するための配列を定義
+    var rowListArray = [Int]()
+    
+    //アイコンのバッジ操作用
+    let application = UIApplication.shared
+
     // 広告ユニットID
-    let AdMobID = "AdMobで取得した広告ID"
+    let AdMobID = "AdMobで取得したID"
+    
     // テスト用広告ユニットID
     let TEST_ID = "ca-app-pub-3940256099942544/2934735716"
     // true:テスト
-    let AdMobTest:Bool = true　//いらないかも
+    let AdMobTest:Bool = true
     
+    //ViewControllerがインスタンス化された時(最初似表示された時)呼ばれるメソッド
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
-        //itemListにToDoItem.Swiftで作ったデータモデルを読み込み、日付(date)順に並べ替える
-        itemList = realm.objects(ToDoItem.self).sorted(byKeyPath: "date")
-        token = realm.observe{ notificaiton,realm in
+        //itemListにToDoItem.Swiftで作ったデータモデルを読み込み、入力された順に表示
+        itemList = realm.objects(ToDoItem.self)
+        
+        token = realm.observe{ [self] notificaiton,realm in
             //realmのデータベースに変更があった場合、tableViewを更新する
-            self.tableView.reloadData()
+            tableView.reloadData()
+            
         }
         
-        
+        //rowListArray重複削除
+        let newListArray = Array(Set(rowListArray))
+        //セルの選択を有効にする(true = 複数選択可)
+        self.tableView.allowsMultipleSelection = true
+        //AdMob広告読み込み
 
+        GADBannerView.adUnitID = AdMobID//テストの際はTEST_IDに変更
+        GADBannerView.rootViewController = self
+        GADBannerView.load(GADRequest())
+        
+        //項目が入っていない部分の区切り線を消す
+        self.tableView.tableFooterView = UIView()
+        
+        //バッジを表示
+        if itemList.count == 0 || itemList.count == newListArray.count {
+            application.applicationIconBadgeNumber = 0
+        } else {
+                application.applicationIconBadgeNumber = itemList.count - newListArray.count
+        }
+      
     }
-    //バナー広告表示用メソッド
-    override func viewDidLayoutSubviews(){    // ★←この関数まるまる追記
-            //  広告インスタンス作成
-            var admobView = GADBannerView()
-            admobView = GADBannerView(adSize:kGADAdSizeBanner)
-            
-            //  広告位置設定(iPhoneX対応)
-            let safeArea = self.view.safeAreaInsets.bottom
-            admobView.frame.origin = CGPoint(x:0, y:self.view.frame.size.height - safeArea - admobView.frame.height)
-            admobView.frame.size = CGSize(width:self.view.frame.width, height:admobView.frame.height)
-            
-            //  広告ID設定
-            admobView.adUnitID = TEST_ID　//シミュレーターの場合はTEST_ID、本番はAdMobIDを設定
-            
-            //  広告表示
-            admobView.rootViewController = self
-            admobView.load(GADRequest())
-            self.view.addSubview(admobView)
+    
+    //viewControllerが表示された時に呼ばれるメソッド(毎回)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tableView.reloadData()
+        
+        //rowListArray重複削除
+        let newListArray = Array(Set(rowListArray))
+        
+        
+        
+        //バッジを表示
+        if itemList.count == 0 || itemList.count == newListArray.count {
+            application.applicationIconBadgeNumber = 0
+        } else {
+                application.applicationIconBadgeNumber = itemList.count - newListArray.count
         }
-
+        
+        //iOS14以降ならトラッキング許可のアラートを表示するメソッドを呼び出す(iPhoneの設定に応じてバターンを用意)
+        if #available(iOS 14, *) {
+                    switch ATTrackingManager.trackingAuthorizationStatus {
+                    //iPhoneで全てのトラッキングが許可されている場合
+                    case .authorized:
+                        print("Allow Tracking")
+                        print("IDFA: \(ASIdentifierManager.shared().advertisingIdentifier)")
+                    //iPhoneで全てのトラッキングが拒否されている場合(アラートも表示されない)
+                    case .denied:
+                        print("拒否")
+                    case .restricted:
+                        print("制限")
+                    //iPhoneでトラッキングに関する設定がされていない場合(アラートを表示して許可を求める)
+                    case .notDetermined:
+                        showRequestTrackingAuthorizationAlert()
+                    @unknown default:
+                        fatalError()
+                    }
+                } else {// iOS14未満
+                    if ASIdentifierManager.shared().isAdvertisingTrackingEnabled {
+                        print("Allow Tracking")
+                        print("IDFA: \(ASIdentifierManager.shared().advertisingIdentifier)")
+                    } else {
+                        print("制限")
+                    }
+                }
+        
+    }
+    ///Alert表示
+        private func showRequestTrackingAuthorizationAlert() {
+            if #available(iOS 14, *) {
+                ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
+                    switch status {
+                    case .authorized:
+                        print("許可されました")
+                        //IDFA取得
+                        print("IDFA: \(ASIdentifierManager.shared().advertisingIdentifier)")
+                    case .denied, .restricted, .notDetermined:
+                        print("拒否されました")
+                    @unknown default:
+                        fatalError()
+                    }
+                })
+            }
+        }
     //TableViewを使うための2つのメソッド
     //①リストの行数を決めるメソッド
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -63,14 +138,77 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         let item = itemList[indexPath.row]
         //リストの左側(項目)に表示する文字列(itemListのrow番目のタイトル)
         cell?.textLabel?.text = item.title
+        //numberOfLines = 0で改行をいくらでもできるようにする
         cell?.textLabel?.numberOfLines = 0
-        //リストの右側(日時)に表示する文字列
-        //InfoHelperから日付を文字列型に変換する処理を読み込む
-        //let dateString = InfoHelper().dateToString(date: item.date)
+        
         cell?.detailTextLabel?.text = InfoHelper().dateToString(date: item.date)
+
         return cell!
+
         }
+
+    //セルがタップされた直後に読み込まれるメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //indexPathからどのセルがタップされたか判定する
+        let cell = tableView.cellForRow(at: indexPath)
+        //取り消し線を追加するの文字列を定義
+        let atr = NSMutableAttributedString(string: (cell?.textLabel?.text)!)
+
+        //rowListArrayに行番号を追加
+        self.rowListArray.append(indexPath.row)
+        //rowListArrayが重複していたら同じ値を削除
+        let newListArray = Array(Set(rowListArray))
+
+       
+        //タップされたセルにチェックボックスを加える
+        cell?.accessoryType = .checkmark
+        //文字色をグレーにする
+        cell?.textLabel?.textColor = .gray
+        //文字に取り消し線を追加する(.strikethroughStyle)、valueで線の太さ、NSMakeRange(0, atr.length)でテキスト全体を指定
+        atr.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 2, range: NSMakeRange(0, atr.length))
+        //取り消し線を反映
+        cell?.textLabel?.attributedText = atr
+
+        //バッジを表示
+        if itemList.count == 0 || itemList.count == newListArray.count {
+            application.applicationIconBadgeNumber = 0
+        } else {
+                application.applicationIconBadgeNumber = itemList.count - newListArray.count
+        }
+ 
+      }
     
+    //もう一度セルをタップした直後(セルの選択解除直後)に読み込まれるメソッド
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        //indexPathからどのセルがタップされたか判定する
+        let cell = tableView.cellForRow(at: indexPath)
+        //取り消し線操作用
+        let atr = NSMutableAttributedString(string: (cell?.textLabel?.text)!)
+        
+        
+        //タップされたセルにチェックボックスを消す
+        cell?.accessoryType = .none
+        //文字色をデフォルトに戻す
+        cell?.textLabel?.textColor = .label
+        //取り消し線を戻す(value:0)
+        atr.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 0, range: NSMakeRange(0, atr.length))
+        //ラベルに反映
+        cell?.textLabel?.attributedText = atr
+        
+        //rowListArrayに入っている行番号を削除
+        rowListArray.removeAll(where: {$0 == indexPath.row})
+
+        //rowListArrayが重複していたら同じ値を削除
+        let newListArray = Array(Set(rowListArray))
+        
+        //バッジを表示
+        if itemList.count == 0 || itemList.count == newListArray.count {
+            application.applicationIconBadgeNumber = 0
+        } else {
+                application.applicationIconBadgeNumber = itemList.count - newListArray.count
+        }
+ 
+    }
     
     
     //tableViewをスワイプできるようにするメソッド
@@ -80,12 +218,89 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     
     //スワイプしたItemを削除するメソッド(InfoHelper.deleteItem)を読み込む
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        //indexPathからどのセルがタップされたか判定する
+        let cell = tableView.cellForRow(at: indexPath)
+        //取り消し線操作用
+        let atr = NSMutableAttributedString(string: (cell?.textLabel?.text)!)
+     
+        //タップされたセルにチェックボックスを消す
+        cell?.accessoryType = .none
+        //文字色をデフォルトに戻す
+        cell?.textLabel?.textColor = .label
+        //取り消し線を戻す(value:0)
+        atr.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 0, range: NSMakeRange(0, atr.length))
+        //ラベルに反映
+        cell?.textLabel?.attributedText = atr
+
+        //rowListArrayに入っている行番号を削除
+        rowListArray.removeAll(where: {$0 == indexPath.row})
+        for i in 0..<rowListArray.count {
+            if rowListArray[i] > indexPath.row {
+            rowListArray[i] = rowListArray[i] - 1
+            }
+        }
+        
+        print("インデックス\(rowListArray)")
+        
         if editingStyle == .delete {
             InfoHelper().deleteItem(item: itemList[indexPath.row], token: token)
             tableView.deleteRows(at: [indexPath], with: .automatic)
         }
+        //rowListArrayが重複していたら同じ値を削除
+        let newListArray = Array(Set(rowListArray))
+        
+        //バッジを表示
+        if itemList.count == 0 || itemList.count == newListArray.count {
+            application.applicationIconBadgeNumber = 0
+        } else {
+                application.applicationIconBadgeNumber = itemList.count - newListArray.count
+        }
+        
     }
+    
 
+    
+    //ゴミ箱ボタンが押されたときの処理
+    @IBAction func deleteButton(_ sender: Any) {
+        
+        //チェックマークが入っているセルがあるか判定
+        if rowListArray.isEmpty == true {
+            print("Enpty")
+        } else {
+            //rowArrayの値を降順に並べ替え
+            rowListArray.sort(by: > )
+            //チェックマークがついたセルを削除
+            for value in rowListArray {
+                //タップされたセルにチェックボックスを消す
+                let cell = tableView.cellForRow(at: [0,value])
+                //取り消し線操作用
+                let atr = NSMutableAttributedString(string: (cell?.textLabel?.text)!)
+                cell?.accessoryType = .none
+                //文字色をデフォルトに戻す
+                cell?.textLabel?.textColor = .label
+                //取り消し線を戻す(value:0)
+                atr.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 0, range: NSMakeRange(0, atr.length))
+                //ラベルに反映
+                cell?.textLabel?.attributedText = atr
+                InfoHelper().deleteItem(item: itemList[value], token: token)
+            }
+            rowListArray.removeAll()
+        }
+        
+        //rowListArrayが重複していたら同じ値を削除
+        let newListArray = Array(Set(rowListArray))
+        
+        //バッジを表示
+        if itemList.count == 0 || itemList.count == newListArray.count {
+            application.applicationIconBadgeNumber = 0
+        } else {
+                application.applicationIconBadgeNumber = itemList.count - newListArray.count
+        }
+        
+        //tableViewを更新する
+        tableView.reloadData()
+    }
+    
 }
 
 
